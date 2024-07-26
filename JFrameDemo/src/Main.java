c697e43386431c90e6c366a25b6baec5f05d154e
玄兴梦影 <1989718921@qq.com>
2024-07-27 00:54:55 +0800

    first commit

---
+++ JFrameDemo/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Welcome");
+        frame.setSize(700, 500);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+}
