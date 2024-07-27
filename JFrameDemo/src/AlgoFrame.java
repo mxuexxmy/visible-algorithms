2fa6c5fbbfd7af3b255f3e66779796ce3968ec93
玄兴梦影 <1989718921@qq.com>
2024-07-27 19:13:12 +0800

    [feat] 2-3 using panel

--- JFrameDemo/src/AlgoFrame.java
+++ JFrameDemo/src/AlgoFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class AlgoFrame extends JFrame {
 
@@ -10,7 +11,9 @@ public class AlgoFrame extends JFrame {
         super(title);
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight;
-        this.setSize(canvasWidth, canvasHeight);
+        AlgoCanvas canvas = new AlgoCanvas();
+        this.setContentPane(canvas);
+        this.pack();
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
@@ -26,5 +29,20 @@ public class AlgoFrame extends JFrame {
 
     public int getCanvasHeight() {
         return canvasHeight;
+    }
+
+    private class AlgoCanvas extends JPanel {
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawOval(50, 50, 300, 300);
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(canvasWidth, canvasHeight);
+        }
+
     }
 }
